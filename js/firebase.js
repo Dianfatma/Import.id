dd18ed3a5cc26a6d41d4b77a5d101e4efa7b2eb6
Emma Paris <eparis@atlassian.com>
2019-09-04 16:47:23 +0700

    commit message goes here

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCr3TcBgINNZpPdIFcb1iP_ROQwKnxLT6Y",
+    authDomain: "lightrees-6fe85.firebaseapp.com",
+    databaseURL: "https://lightrees-6fe85.firebaseio.com",
+    projectId: "lightrees-6fe85",
+    storageBucket: "",
+    messagingSenderId: "958464162259",
+    appId: "1:958464162259:web:eaf499bdbfdc95d2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
